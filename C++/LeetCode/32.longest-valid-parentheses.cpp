be1cf42e90444b75724ae69e51f2d17421f94611
hakulachen <i@hakula.xyz>
2024-01-25 10:36:37 +0800

    refactor(c++): optimize LeetCode 32

--- C++/LeetCode/32.longest-valid-parentheses.cpp
+++ C++/LeetCode/32.longest-valid-parentheses.cpp
@@ -12,22 +12,17 @@
 class Solution {
  public:
   int longestValidParentheses(const std::string& s) {
-    int result = 0, begin = 0;
-    std::stack<int> stack;
+    int result = 0;
+    std::stack<int> stack{{-1}};
     for (int i = 0; i < s.size(); ++i) {
       if (s[i] == '(') {
         stack.push(i);
       } else {
+        stack.pop();
         if (stack.empty()) {
-          begin = i + 1;
-        } else {
-          stack.pop();
-          if (stack.empty()) {
-            result = std::max(i - begin + 1, result);
-          } else {
-            result = std::max(i - stack.top(), result);
-          }
+          stack.push(i);
         }
+        result = std::max(i - stack.top(), result);
       }
     }
     return result;
